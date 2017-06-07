055c7c92eed95d6d11861f62e3bbf463fefe088e
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2017-06-07 14:51:59 -0500

    Update Package.swift for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-  name: "Mozart"
+  name: "Mozart",
+  products: [
+    .library(name: "Mozart", targets: ["Mozart"]),
+  ],
+  targets: [
+    .target(name: "Mozart"),
+  ]
 )
